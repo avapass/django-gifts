bdaf176738edd21b2313c91949825eb62d74a561
Ana Pas <avagreen.code@yahoo.com>
2024-03-14 19:13:49 +0200

    Add model Product

--- gifts_rec/app/models.py
+++ gifts_rec/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    price = models.FloatField()
+    description = models.CharField(max_length=100)
+    image = models.FileField(null=True, blank=True)
