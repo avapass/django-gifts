c66047198dc9c6c460ce8633c35b6a9e51c666a6
Ana Pas <avagreen.code@yahoo.com>
2024-03-14 19:48:44 +0200

    products_list + add in admin

--- gifts_rec/app/views.py
+++ gifts_rec/app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Product
 
 # Create your views here.
+
+def products_list(request):
+    products = Product.objects.all()
+    products_fixed = [
+        f"<li>{product.title} - {product.price}</li>"
+        for product in products
+    ]
+    response_string = "<ol>"
+    response_string += "".join(products_fixed)
+    response_string += "</ol>"
+
+    return HttpResponse(response_string)
